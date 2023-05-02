23f2c2e864588ca75c870130f29775518ace7184
JavyDaniel <danieljavy@gmail.com>
2023-05-02 03:54:53 -0600

    aagregando form para los productos nuevos

--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -1,10 +1,11 @@
 import Layout from "@/src/components/Layout";
+import Link from "next/link";
 
 export default function Productos() {
 
     return(
         <Layout>
-            Productos
+            <Link className={"bg-blue-600 text-white rounded-md py-1 px-2"} href={"/Productos/Nuevo"}>Agregar nuevo producto</Link>
         </Layout>
     );
 
---
+++ src/pages/Productos/Nuevo.js
@@ -0,0 +1,45 @@
+import Layout from "@/src/components/Layout";
+import {useState} from "react";
+import axios from "axios";
+
+export default function Nuevo() {
+
+    const [nombre, setNombre] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [precio, setPrecio] = useState('');
+    
+    async function guardarProducto() {
+        const data = {nombre,descripcion,precio}
+        await axios.post('', data)
+    }
+    return (
+        <Layout>
+
+            <form onSubmit={guardarProducto}>
+                <h1>Nuevo Producto</h1>
+                <label>Nombre del producto</label>
+                <input
+                    type={"text"}
+                    placeholder={"nombre del producto"}
+                    value={nombre}
+                    onChange={ev => setNombre(ev.target.value)}
+                />
+                <label>Descripcion</label>
+                <textarea
+                    placeholder={"descripcion"}
+                    value={descripcion}
+                    onChange={ev => setDescripcion(ev.target.value)}
+                ></textarea>
+                <label>Precio (MXN)</label>
+                <input
+                    type={"number"}
+                    placeholder={"precio"}
+                    value={precio}
+                    onChange={ev => setPrecio(ev.target.value)}
+                />
+                <button type={"submit"} className={"btn-primary"}>Guardar</button>
+            </form>
+        </Layout>
+    );
+
+}
